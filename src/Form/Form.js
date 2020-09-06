3e544be56735303d1de94d3c913c56d40b3804c1
Naomi Ware <nware1066@yahoo.com>
2020-09-06 16:49:09 -0600

    Add constructor and JSX template for Form

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './Form.css';
 
+class form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      city: ''
+    }
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          type='text'
+          placeholder='City'
+          name='city'
+          value='{this.state.city}'
+        />
+
+        <button>SUBMIT</button>
+      </form>
+    )
+  }
+}
 
 
 export default Form;
