8685d77f95734d8dcca49700410c451550ba4f65
Naomi Ware <nware1066@yahoo.com>
2020-09-06 16:42:58 -0600

    Set up App class and Form Class

---
+++ src/App/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Form from './Form';
+import AllWeather from './AllWeather';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super() {
+      this.state = {
+        weather: []
+      }
+
+    }
+  }
+
+    render() {
+      return (
+        <main className='App'>
+        </main>
+
+      )
+    }
+  )
+}
+
+
+export default App;
