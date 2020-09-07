3f5bcd493eb966f652ed2b36b7b133886466b3a0
Naomi Ware <nware1066@yahoo.com>
2020-09-06 20:31:12 -0600

    Add variable names to DailyWeather component

--- src/DailyWeather/DailyWeather.js
+++ src/DailyWeather/DailyWeather.js
@@ -4,12 +4,13 @@ import './DailyWeather.css';
 function DailyWeather(props) {
   return (
     <article className='DailyWeather'>
-      <h1>Date</h1>
-      <p>Temp</p>
-      <p>Skies</p>
-      <p>windspeed</p>
-      <p>Chance of Rain</p>
-      <p>Humidity</p>
+      <h1>Date: {props.list.dt}</h1>
+      <p>High Temp: {props.list.main.temp_max}</p>
+      <p>Low Temp: {props.list.main.temp_min}</p>
+      <p>Skies: {props.list.clouds.all}</p>
+      <p>windspeed: {props.list.wind.speed}</p>
+      <p>Chance of Rain: {props.list.pop}</p>
+      <p>Humidity: {props.list.main.humidity}</p>
     </article>
   )
 }
